7fcb4268c918ca87529a572c8da7fcb4f29985d3
dolelongan <dolelongan@gmail.com>
2023-02-25 10:57:58 +0700

    LandDistance (Medium) using Queue FIFO algo (DONE)

--- src/main/LandDistance.java
+++ src/main/LandDistance.java
@@ -1,47 +1,41 @@
 package main;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class LandDistance {
+class LandDistance {
     public int maxDistance(int[][] grid) {
         int width = grid.length;
         Queue<int[]> landQueue = new LinkedList<>();
-        ArrayList<int[]> seaQueue = new ArrayList<>();
         for (int i = 0; i < width * width; i++) {
             int row = i / width; // determines row
             int column = i % width; // determines column
             if (grid[row][column] == 1) {
                 landQueue.add(new int[]{row, column});
-            } else if (grid[row][column] == 0) {
-                seaQueue.add(new int[]{row, column});
             }
             // testing output
-//            System.out.println("Value[" + grid[row][column] + "] Row[" + row + "] Column[" + column + "]");
         }
-        if (landQueue.isEmpty() || seaQueue.isEmpty()) {
+        if (landQueue.isEmpty() || landQueue.size() == width * width) {
             return -1;
         }
         Iterator itr = landQueue.iterator();
         int[] land;
-        int distance = 0;
+        int distance = -1;
         java.util.function.BiFunction<Integer, Integer, Boolean> checkAdjacent = (rowIndex, colIndex) -> {
             if(grid[rowIndex][colIndex] == 0) {
                 grid[rowIndex][colIndex] = 1;
-                // TODO: remove the object from the seaArray
                 return true;
             }
             return false;
         };
-        int rotation = landQueue.size(); // a rotation is the size of the queue for each generation
-        int count = -1; // count the number of "visited" lands have been removed/polled
+        int rotation = landQueue.size();
+        int count =0;
         while (itr.hasNext()) {
-            count++;
             // land[0] is row position, land[1] is column position
             land = landQueue.poll(); // remove and return the first element of the queue
             int row = land[0];
             int col = land[1];
-            assert land != null; // testing
-            // System.out.println("Value[" + land + "] Row[" + row + "] Column[" + col + "]"); // testing
             // adjacent 1
             if (row - 1 >= 0) { // top row NOT OutOfBounds
                 if (checkAdjacent.apply(row - 1, col)) { // adjacent, (0,1)
@@ -60,7 +54,6 @@ public class LandDistance {
                     landQueue.add(new int[]{row+1, col});
                 }
             }
-            // adjacent 4
             if (col + 1 < width) { // right column NOT OutOfBounds
                 if (checkAdjacent.apply(row, col + 1)) { // adjacent, (1,2)
                     landQueue.add(new int[]{row, col + 1});
@@ -68,14 +61,13 @@ public class LandDistance {
             }
             // update the iterator for the queue
             itr = landQueue.iterator();
+            count++;
             if (count==rotation) { // everytime the queue completes its generation
                 distance++;
                 count = 0;
                 rotation = landQueue.size();
             }
-            // TODO: return distance if the seaArray is empty
         }
-        System.out.println("Sea Queue Length: "+seaQueue.size());
         return distance;
     }
 }
--- test/java/main/LandDistanceTest.java
+++ test/java/main/LandDistanceTest.java
@@ -10,15 +10,32 @@ class LandDistanceTest {
     void maxDistance() {
         LandDistance test1 = new LandDistance();
         int case1;
-        case1=test1.maxDistance(new int[][]{{1,0,1}, {0,0,0}, {1,0,1}});
+        case1=test1.maxDistance(new int[][]{
+                {1,0,1},
+                {0,0,0},
+                {1,0,1}});
         System.out.println("Output: "+case1);
         System.out.println("Expected: 2, "+(case1==2));
+    }
+    @Test
+    void maxDistance1_1() {
+        LandDistance test1_1 = new LandDistance();
+        int case1_1;
+        case1_1=test1_1.maxDistance(new int[][]{
+                {1,0,0},
+                {0,0,0},
+                {0,0,0}});
+        System.out.println("Output: "+case1_1);
+        System.out.println("Expected: 4, "+(case1_1==4));
     }
     @Test
     void maxDistance2() {
         LandDistance test2 = new LandDistance();
         int case2;
-        case2=test2.maxDistance(new int[][]{{3,0,3}, {0,0,0}, {3,0,3}});
+        case2=test2.maxDistance(new int[][]{
+                {3,0,3},
+                {0,0,0},
+                {3,0,3}});
         System.out.println("Output: "+case2);
         System.out.println("Expected: -1, "+(case2==-1));
     }
